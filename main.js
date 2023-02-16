8a4880912929332db46f361deca648bd8babe4c5
BABUTSIDZ3 <babutsidzegia7@gmail.com>
2023-02-16 21:39:37 +0400

    main

---
+++ main.js
@@ -0,0 +1,8 @@
+let bar=document.getElementById('bar')
+let headerul=document.getElementById('header-ul')
+bar.addEventListener('click',function(){
+// if(headerul.classList.contains('show'))
+// {headerul.classList.remove('show')}
+// else{headerul.classList.toggle('show')}
+headerul.classList.toggle('show')
+})
