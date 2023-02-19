26c058993139d6a0672ca300b77ad1096cf6b4cc
BABUTSIDZ3 <babutsidzegia7@gmail.com>
2023-02-19 09:30:16 +0400

    main

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
 let bar=document.getElementById('bar')
 let headerul=document.getElementById('header-ul')
 bar.addEventListener('click',function(){
-// if(headerul.classList.contains('show'))
-// {headerul.classList.remove('show')}
-// else{headerul.classList.toggle('show')}
 headerul.classList.toggle('show')
 })
