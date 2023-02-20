f6824a83aa1d4341ca0eb4f5a7818b5f9e7d9b0a
BABUTSIDZ3 <babutsidzegia7@gmail.com>
2023-02-20 21:32:50 +0400

    main

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-let bar=document.getElementById('bar')
-let headerul=document.getElementById('header-ul')
-bar.addEventListener('click',function(){
-headerul.classList.toggle('show')
+let headerul = document.getElementById('header-ul')
+let bars = document.querySelector('.fa-bars')
+bars.addEventListener('click', function () {
+    headerul.classList.toggle('show')
+    bars.classList.toggle("fa-x")
 })
+
+
