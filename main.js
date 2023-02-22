5954c19582354bd7d1a604298936ead1b24bdc93
BABUTSIDZ3 <babutsidzegia7@gmail.com>
2023-02-22 19:20:53 +0400

    main

--- main.js
+++ main.js
@@ -2,13 +2,14 @@ let headerul = document.getElementById('header-ul')
 let slide=document.querySelectorAll('.slide')
 let btn_right=document.getElementById('right')
 let btn_left=document.getElementById('left')
+let bar=document.querySelector('.fa-bars')
+let sec3right=document.getElementById('sec-3-right')
+let sec3left=document.getElementById('sec-3-left')
+let slide1=document.getElementById('sec-3-div1')
+let slide2=document.getElementById('sec-3-div2')
 let counter=0
-if(btn_left.style.backgroundColor=='red'){
-    console.log('ppp')
-}else{console.log('ara')}
 slide.forEach((value,index)=>{
     value.style.transform=`translate(${index*300}%)` // 0 100 200
-    console.log(value)
 })
 btn_right.addEventListener('click',()=>{
     if(counter==slide.length-1){
@@ -18,7 +19,6 @@ btn_right.addEventListener('click',()=>{
     }
     slide.forEach((value, index)=>{
         value.style.transform=`translate(${(index-counter)*300}%)` // 0 100 200
-        console.log(value)
     })
 })
 btn_left.addEventListener('click',()=>{
@@ -27,11 +27,30 @@ btn_left.addEventListener('click',()=>{
     }else{
         counter--
     }
-    console.log(counter)
     slide.forEach((value, index)=>{
         value.style.transform=`translate(${(index-counter)*300}%)` // 100
-        console.log(value)
     })
 })
+bar.addEventListener('click',function(){
+    headerul.classList.toggle('show')
+    bar.classList.toggle('fa-x')
+})
+sec3right.addEventListener('click',function(){
+    sec3left.style.display='block'
+    sec3right.style.display='none'
+    slide1.classList.remove('slideshow')
+slide1.classList.toggle('slidehide')
+slide2.classList.remove('slidehide')
+slide2.classList.toggle('slideshow')
+})
 
+sec3left.addEventListener('click',function(){
+    sec3left.style.display='none'
+    sec3right.style.display='block'
+    slide1.classList.toggle('slideshow')
+    slide1.classList.remove('slidehide')
+slide2.classList.toggle('slidehide')
+slide2.classList.remove('slideshow')
+
+})
 
