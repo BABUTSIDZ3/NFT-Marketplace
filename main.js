d3ca3e1859577e166a99a2da583590c9731ac8e0
BABUTSIDZ3 <babutsidzegia7@gmail.com>
2023-02-22 14:30:03 +0400

    main

--- main.js
+++ main.js
@@ -1,8 +1,37 @@
 let headerul = document.getElementById('header-ul')
-let bars = document.querySelector('.fa-bars')
-bars.addEventListener('click', function () {
-    headerul.classList.toggle('show')
-    bars.classList.toggle("fa-x")
+let slide=document.querySelectorAll('.slide')
+let btn_right=document.getElementById('right')
+let btn_left=document.getElementById('left')
+let counter=0
+if(btn_left.style.backgroundColor=='red'){
+    console.log('ppp')
+}else{console.log('ara')}
+slide.forEach((value,index)=>{
+    value.style.transform=`translate(${index*300}%)` // 0 100 200
+    console.log(value)
+})
+btn_right.addEventListener('click',()=>{
+    if(counter==slide.length-1){
+        counter=0
+    }else{
+        counter++
+    }
+    slide.forEach((value, index)=>{
+        value.style.transform=`translate(${(index-counter)*300}%)` // 0 100 200
+        console.log(value)
+    })
+})
+btn_left.addEventListener('click',()=>{
+    if(counter==0){
+        counter=slide.length-1
+    }else{
+        counter--
+    }
+    console.log(counter)
+    slide.forEach((value, index)=>{
+        value.style.transform=`translate(${(index-counter)*300}%)` // 100
+        console.log(value)
+    })
 })
 
 
